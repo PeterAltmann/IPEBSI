94e8b31a17ec166d65597b3c17d34d1082885558
PeterAltmann <30799110+PeterAltmann@users.noreply.github.com>
2019-11-01 13:15:43 +0100

    Update gitMultiSig.sol
    Changed the solidity version.
    Added counter for total number of signers.
    Removed signature counter and replaced with a hasSigned.length

--- gitMultiSig.sol
+++ gitMultiSig.sol
@@ -1,11 +1,11 @@
-pragma solidity ^0.5.11;
+pragma solidity ^0.5.12;
 
 /// @title A multisig inspired git commit approver
 /// @author Peter Altmann
 /// @notice This contract is meant to showcase some basic options
 /// @dev The setSigner function should be replaced with a proxy that calls a collaborator management contract, which returns address[] and superUser address.
 contract gitMultiSig {
-    uint public signatures;
+    uint public signers;
     address[] notSigned;
     address[] hasSigned;
     mapping (address => bool) signList;
@@ -21,18 +21,19 @@ contract gitMultiSig {
         else {
             signList[msg.sender] = true;
             notSigned.push(msg.sender);
+            signers ++;
         }
     }
 
-    function getNotSigned() public view returns(address[] memory){
+    function getNotSigned() public view returns(address[] memory, uint hasCount){
         /// @return array of addresses who have not yet signed the git commit
-        return notSigned;
+        return (notSigned, notSigned.length);
     }
 
-    function getHasSigned() public view returns(address[] memory){
+    function getHasSigned() public view returns(address[] memory, uint hasCount){
         /// @return array of addresses who have signed the git commit
         /// @param hasSigned address array with addresses who have signed off on the git commit.
-        return hasSigned;
+        return (hasSigned, hasSigned.length);
     }
 
     function createSignature() public returns(string memory Vote){
@@ -40,11 +41,9 @@ contract gitMultiSig {
         /// @dev deletion in Solidity sets memory address associated with index to 0x00...0. Other removal mechanism exists, but delete is most simple.
         /// @dev signList[address] == true; can be an alternative to for loop.
         /// @dev no check implemented for trying to create multiple signatures.
-        /// @param signatures is the counter.
         /// @returns vote outcome
         for (uint i = 0; i < notSigned.length; i++) {
             if (msg.sender == notSigned[i]) {
-                signatures ++;
                 hasSigned.push(msg.sender);
                 delete notSigned[i];
                 return (" casted");
@@ -61,8 +60,8 @@ contract gitMultiSig {
     }
 
     function gitMultiSigCommit(uint _m) public view returns(bool gitCommitted) {
-        /// @notice function performs an m of n test OR a super user signed test. if _m == hasSigned.lenth then that means that everyone has to sign.
-        if (_m <= hasSigned.length || superUserSigned == true) {
+        /// @notice function performs an m of n test OR a super user signed test. if _m == signers then that means that everyone has to sign.
+        if (_m <= signers || superUserSigned == true) {
             return true;
         }
         else {
