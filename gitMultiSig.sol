accbb1350d4e5dc2ac056d61956eb70bf58b5a98
PeterAltmann <30799110+PeterAltmann@users.noreply.github.com>
2019-11-01 14:00:15 +0100

    Update gitMultiSig.sol
    Updated with modifiers.
    Reduced redundant code.

--- gitMultiSig.sol
+++ gitMultiSig.sol
@@ -5,29 +5,35 @@ pragma solidity ^0.5.12;
 /// @notice This contract is meant to showcase some basic options
 /// @dev The setSigner function should be replaced with a proxy that calls a collaborator management contract, which returns address[] and superUser address.
 contract gitMultiSig {
-    uint public signers;
+    uint signers;
     address[] notSigned;
     address[] hasSigned;
-    mapping (address => bool) signList;
+    mapping (address => bool) public signList;
+    mapping (address => bool) public signedList;
     bool public superUserSigned;
 
-    function setSigner() public {
+    modifier testSigner() {
+        require (!signList[msg.sender]);
+        _;
+    }
+    modifier testSigned() {
+        require (!signedList[msg.sender]);
+        _;
+    }
+    
+    function setSigner() testSigner public {
         /// @notice since we do not import "mapping (address => colaboratorStruct) collaborators", we need a way to create a list of collaborators.
         /// @param signList Solidity cannot loop through arrays, so we need to create a mapping to be able to check if someone is already registered.
         /// @param notSigned is an address array with addresses who have not yet signed off on the git commit.
-        if (signList[msg.sender] == true) {
-            revert();
-        }
-        else {
-            signList[msg.sender] = true;
-            notSigned.push(msg.sender);
-            signers ++;
-        }
+        /// @param signers countes total number of signers.
+        signList[msg.sender] = true;
+        notSigned.push(msg.sender);
+        signers ++;
     }
 
     function getNotSigned() public view returns(address[] memory, uint hasCount){
         /// @return array of addresses who have not yet signed the git commit
-        return (notSigned, notSigned.length);
+        return (notSigned, signers - hasSigned.length);
     }
 
     function getHasSigned() public view returns(address[] memory, uint hasCount){
@@ -35,13 +41,14 @@ contract gitMultiSig {
         /// @param hasSigned address array with addresses who have signed off on the git commit.
         return (hasSigned, hasSigned.length);
     }
-
-    function createSignature() public returns(string memory Vote){
+    
+    function createSignature() public testSigned returns(string memory Vote){
         /// @notice checks for implements a simple counter for the m of n part; adds signer to hasSigned; deletes the index of the signer;
         /// @dev deletion in Solidity sets memory address associated with index to 0x00...0. Other removal mechanism exists, but delete is most simple.
-        /// @dev signList[address] == true; can be an alternative to for loop.
+        /// @dev signList[address] == true; can be an alternative to for loop but introduces delete problems. Internal function call can be an alternative.
         /// @dev no check implemented for trying to create multiple signatures.
         /// @returns vote outcome
+        signedList[msg.sender] = true;
         for (uint i = 0; i < notSigned.length; i++) {
             if (msg.sender == notSigned[i]) {
                 hasSigned.push(msg.sender);
@@ -50,22 +57,16 @@ contract gitMultiSig {
             }
             else continue;
         }
-        return (" not casted");
     }
 
     function createSuperUserSignature() public {
         /// @notice sets a signature from a superuser, i.e., a user with priveleges. For illustrative purposes the superuser address check always evaluates as true.
         /// @param superUserSigned is a bool that
-        if (msg.sender == msg.sender) superUserSigned = true;
+        superUserSigned = (msg.sender == msg.sender);
     }
 
     function gitMultiSigCommit(uint _m) public view returns(bool gitCommitted) {
         /// @notice function performs an m of n test OR a super user signed test. if _m == signers then that means that everyone has to sign.
-        if (_m <= signers || superUserSigned == true) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return (_m <= signers || superUserSigned);
     }
 }
