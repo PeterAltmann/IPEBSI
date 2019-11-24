426542152eaefc38a48d8da59314157696528e5f
PeterAltmann <30799110+PeterAltmann@users.noreply.github.com>
2019-11-24 23:50:40 +0100

    added a require for the createSignature()
    Past version allowed an address to run the createSignature() functions before setSigner(). This lead to a state where that address would not be able to sign after correctly adding their address to the signer list.

--- collaboratorManager.sol
+++ collaboratorManager.sol
@@ -1,81 +1,73 @@
 pragma solidity ^0.5.12;
 
-/// @title collaboratorManager
-/// @notice this contract manages a list of collaborators for a project
-contract collaboratorManager {
-    
-    address superOwner;
-    constructor() public {
-        superOwner = msg.sender;
+/// @title A multisig inspired git commit approver
+/// @author Peter Altmann
+/// @notice This contract is meant to showcase some basic options
+/// @dev The setSigner function should be replaced with a proxy that calls a collaborator management contract, which returns address[] and superUser address.
+contract gitMultiSig {
+    uint signers;
+    address[] notSigned;
+    address[] hasSigned;
+    mapping (address => bool) public signList;
+    mapping (address => bool) public signedList;
+    bool public superUserSigned;
+
+    modifier testSigner() {
+        require (!signList[msg.sender]);
+        _;
     }
-    modifier onlyOwner {
-        require(msg.sender == superOwner);
+    modifier testSigned() {
+        require (!signedList[msg.sender]);
         _;
     }
     
-    /// @param organization is the name of the collaborator's organization
-    /// @param fName is first name of collaborator representative
-    /// @param lName is last name of collaborator representative
-    /// @param listPointer is the index of the entityList variables
-    /// @param entityStructs maps the addresses of the collaborators with the EntityStruct information
-    /// @param entityList is the set of all collaborators
-    struct EntityStruct {
-        string organization;
-        string fName;
-        string lName;
-        uint listPointer;
-    }
-    mapping(address => EntityStruct) public entityStructs;
-    address[] public entityList;
-    
-    /// @notice creates a new collaborator unless a collaborator address already is registered
-    function newEntity(address _address, string memory organization, string memory fName, string memory lName) public payable returns(bool creationSuccess){
-        if(isEntity(_address)) revert();
-        entityStructs[_address].organization = organization;
-        entityStructs[_address].fName = fName;
-        entityStructs[_address].lName = lName;
-        entityStructs[_address].listPointer = entityList.push(_address) - 1;
-        return true;
+    function setSigner() testSigner public {
+        /// @notice since we do not import "mapping (address => colaboratorStruct) collaborators", we need a way to create a list of collaborators.
+        /// @param signList Solidity cannot loop through arrays, so we need to create a mapping to be able to check if someone is already registered.
+        /// @param notSigned is an address array with addresses who have not yet signed off on the git commit.
+        /// @param signers countes total number of signers.
+        signList[msg.sender] = true;
+        notSigned.push(msg.sender);
+        signers ++;
     }
 
-    /// @notice updates an existing collaborator unless an unregistered address is specified
-    function updateEntity(address _address, string memory organization, string memory fName, string memory lName) public payable returns(bool updateSuccess) {
-        if (!isEntity(_address)) {
-            revert();
+    function getNotSigned() public view returns(address[] memory, uint hasCount){
+        /// @return array of addresses who have not yet signed the git commit
+        return (notSigned, signers - hasSigned.length);
+    }
+
+    function getHasSigned() public view returns(address[] memory, uint hasCount){
+        /// @return array of addresses who have signed the git commit
+        /// @param hasSigned address array with addresses who have signed off on the git commit.
+        return (hasSigned, hasSigned.length);
+    }
+    
+    function createSignature() public testSigned returns(string memory Vote){
+        /// @notice checks for implements a simple counter for the m of n part; adds signer to hasSigned; deletes the index of the signer;
+        /// @dev deletion in Solidity sets memory address associated with index to 0x00...0. Other removal mechanism exists, but delete is most simple.
+        /// @dev signList[address] == true; can be an alternative to for loop but introduces delete problems. Internal function call can be an alternative.
+        /// @dev no check implemented for trying to create multiple signatures.
+        /// @returns vote outcome
+        require (signList[msg.sender]);
+        signedList[msg.sender] = true;
+        for (uint i = 0; i < notSigned.length; i++) {
+            if (msg.sender == notSigned[i]) {
+                hasSigned.push(msg.sender);
+                delete notSigned[i];
+                return (" casted");
+            }
+            else continue;
         }
-        else if (msg.sender != _address && msg.sender != superOwner) {
-            revert();
-        }
-        entityStructs[_address].organization = organization;
-        entityStructs[_address].fName = fName;
-        entityStructs[_address].lName = lName;
-        return true;
     }
 
-    /// @notice deletes an existing collaborator
-    /// @dev the function works by overwriting the address to be deleted with the address at the end of the list and then reducing the list size by 1
-    function deleteEntity(address _address) public payable returns(bool deleteSuccess) {
-        if(!isEntity(_address)) revert();
-        uint rowToDelete = entityStructs[_address].listPointer;
-        address keyToMove = entityList[entityList.length-1];
-        entityList[rowToDelete] = keyToMove;
-        entityStructs[keyToMove].listPointer = rowToDelete;
-        entityList.length--;
-        return true;
-    }
-    
-    /// @dev we can return the entityList to another contract if we want to make this into a proxy contract
-    function getCollaborators() public view returns(address[] memory){
-        return entityList;
+    function createSuperUserSignature() public {
+        /// @notice sets a signature from a superuser, i.e., a user with priveleges. For illustrative purposes the superuser address check always evaluates as true.
+        /// @param superUserSigned is a bool that
+        superUserSigned = (msg.sender == msg.sender);
     }
 
-    function isEntity(address _address) public view returns(bool entityExists) {
-        if(entityList.length == 0) return false;
-        return (entityList[entityStructs[_address].listPointer] == _address);
+    function gitMultiSigCommit(uint _m) public view returns(bool gitCommitted) {
+        /// @notice function performs an m of n test OR a super user signed test. if _m == signers then that means that everyone has to sign.
+        return ((_m <= hasSigned.length) || superUserSigned);
     }
-
-    function getEntityCount() public view returns(uint entityCount) {
-        return entityList.length;
-    }
-    
 }
